315676f0c6a9a03de4f3dea3825b6c11d8364885
MacksimBondarew <112969071+MacksimBondarew@users.noreply.github.com>
2024-03-04 22:15:39 +0100

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Nav from "./components/Nav";
 import {
   Hero,
   PopularProducts,
@@ -11,15 +12,17 @@ import {
 
 const App = () => (
     <main className="relative">
+        <Nav />
         <section className="xl:padding-1 wide:padding-r padding-b">
-            Hero
+            <Hero />
         </section>
-        <section className="padding">PopularProducts</section>
-        <section className="padding">SuperQuality</section>
-        <section className="padding-x">Services</section>
-        <section className="padding bg-pale-blue">CustomerReviews</section>
-        <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
-        <section className="padding-x padding-t pb-8">Footer</section>
+        <section className="padding"><PopularProducts /></section>
+        <section className="padding"><SuperQuality /></section>
+        <section className="padding-x"><Services /></section>
+        <section className="padding"><SpecialOffer /></section>
+        <section className="padding bg-pale-blue"><CustomerReviews /></section>
+        <section className="padding-x sm:py-32 py-16 w-full"><Subscribe /></section>
+        <section className="padding-x padding-t pb-8"><Footer /></section>
     </main>
 );
 
