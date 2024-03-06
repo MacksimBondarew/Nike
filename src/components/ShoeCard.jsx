e4ab12b1177c64e882094e38a97972ab5fa6674d
MacksimBondarew <112969071+MacksimBondarew@users.noreply.github.com>
2024-03-06 22:07:41 +0100

    add

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,7 +1,29 @@
 const ShoeCard = ({ imageURL, changeBigShoeImage, bigShoeImg }) => {
-  return (
-    <div className={`border-2 rounded-xl ${bigShoeImage === imageURL ? "border-coral-red" : "border-transparent"}`}></div>
-  )
-}
+    const handleClick = () => {
+        if (bigShoeImg !== imageURL) {
+            changeBigShoeImage(imageURL.bigShoe);
+        }
+    };
+    return (
+        <div
+            className={`border-2 rounded-xl ${
+                bigShoeImg === imageURL.bigShoe
+                    ? "border-coral-red"
+                    : "border-transparent"
+            } cursor-pointer max-sm:flex-1`}
+            onClick={handleClick}
+        >
+            <div className="flex justify-center items-center bg-card bg-center sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
+                <img
+                    src={imageURL.thumbnail}
+                    alt="shoe collection"
+                    width={127}
+                    height={103}
+                    className="object-contain"
+                />
+            </div>
+        </div>
+    );
+};
 
-export default ShoeCard
+export default ShoeCard;
